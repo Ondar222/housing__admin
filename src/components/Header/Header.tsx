83065b0b1a5eef6f1a999421695afd31dd0b642b
suldemka1 <appex@me.com>
2024-03-14 01:35:03 +0700

    components example

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import styles from "./styles.module.css"
+
+const Header: FC = () => {
+    return <div className={styles.header}></div>
+}
+
+export { Header }
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react"
+import styles from "./styles.module.css"
+
+const Sidebar: FC = () => {
+    return <div className={styles.sidebar}></div>
+}
+
+export { Sidebar }
